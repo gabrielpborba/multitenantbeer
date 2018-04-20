4870eee91301cdfedfb6b3860752b2d02fda6b22
Gabiel Borba <pastregabriel@hotmail.com>
2018-04-20 18:59:08 -0300

    INSERTS BANCO

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,7 @@ INSERT INTO DATASOURCECONFIG VALUES (1, 'org.h2.Driver', 'jdbc:h2:mem:coloradoDS
 INSERT INTO DATASOURCECONFIG VALUES (2, 'org.h2.Driver', 'jdbc:h2:mem:microxDS', 'Micro X', 'sa', '', true);
 INSERT INTO DATASOURCECONFIG VALUES (3, 'org.h2.Driver', 'jdbc:h2:mem:invictaDS', 'Invicta', 'sa', '', true);
 
-INSERT INTO BEER (nome, tipo) VALUES ('SKOL', 'porcaria');
+INSERT INTO BEER (nome, tipo) VALUES ('SKOL', 'PILSEN');
+INSERT INTO BEER (nome, tipo) VALUES ('ITAIPAVA', 'PILSEN');
+INSERT INTO BEER (nome, tipo) VALUES ('BUDWEISER', 'PURO MALTE');
+INSERT INTO BEER (nome, tipo) VALUES ('EISENBAHN', 'PURO MALTE');
